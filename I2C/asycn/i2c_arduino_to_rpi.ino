5b9ba3ab5064b2af023c4a92c2bbe10d7c112101
GaryChenMesser <gary9192631770@gmail.com>
2017-10-26 20:27:23 +0800

    Add files via upload

---
+++ I2C/asycn/i2c_arduino_to_rpi.ino
@@ -0,0 +1,52 @@
+#include <Wire.h>
+#define SLAVE_ADDRESS 0x04
+int number = 0;
+int state = 0;
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(8,OUTPUT);
+  digitalWrite(8, HIGH);
+  Serial.begin(9600); // start serial for output
+  // initialize i2c as slave
+  Wire.begin(SLAVE_ADDRESS);
+  // define callbacks for i2c communication
+  Wire.onReceive(receiveData);
+  Wire.onRequest(sendData);
+  Serial.println("Ready!");
+}
+void loop() {
+  delay(100);
+} 
+// callback for received data
+void receiveData(){
+  while(Wire.available()) {
+    number = Wire.read();
+    Serial.print("data received: vhevk");
+    Serial.println(number);
+    //sendData();
+  }
+}
+// callback for sending data
+void sendData(){
+  Wire.write(number);
+}
+
+void ISRR(){
+  digitalWrite(8, LOW);
+  Serial.println("LOW");
+  delayMicroseconds(400);
+  digitalWrite(8, HIGH);
+  
+}
+
+void serialEvent(){
+  while(Serial.available()){
+    number = Serial.read();
+    number -= 48;
+    Serial.print("data to send: ");
+    Serial.println(number);
+    ISRR();
+  }
+}
+
+
